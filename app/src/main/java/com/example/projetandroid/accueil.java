ad67dc8c961989f2d65c95fced0ca043e3ac1192
Martial_GESLIN <martial.geslin@hotmail.com>
2022-02-23 18:04:58 +0100

    accueil

---
+++ app/src/main/java/com/example/projetandroid/accueil.java
@@ -0,0 +1,38 @@
+package com.example.projetandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                retourMain(view);
+
+            }
+        });
+    }
+
+    public void retourMain(View c){
+        Intent intent = new Intent(accueil.this, MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
