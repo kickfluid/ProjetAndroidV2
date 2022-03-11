567c9b05ac2bcf6b4009934ae66025a0acbef332
BATISTA Fabrice <fabrice.batista10@gmail.com>
2022-03-11 09:04:42 +0100

    pagefav

---
+++ app/src/main/java/com/example/projetandroid/AdapterP.java
@@ -0,0 +1,44 @@
+package com.example.projetandroid;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.projetandroid.R;
+
+import java.util.ArrayList;
+
+class AdapterP extends ArrayAdapter<String> {
+
+    private Integer[] tab_images_pour_la_liste = {
+            R.drawable.common_google_signin_btn_text_dark
+    };
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        textView.setText(getItem(position));
+
+        if(convertView == null )
+            imageView.setImageResource(tab_images_pour_la_liste[position]);
+        else
+            rowView = (View)convertView;
+
+        return rowView;
+    }
+
+    public AdapterP(Context context, ArrayList<String> values) {
+        super(context, R.layout.rowlayout, values);
+    }
+}
--- app/src/main/java/com/example/projetandroid/Description.java
+++ app/src/main/java/com/example/projetandroid/Description.java
@@ -4,25 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class Description extends AppCompatActivity {
-
+    private ListView lv;
+    public AdapterP adapter;
+    public ArrayList<String> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+        ImageButton fav = (ImageButton) findViewById(R.id.favoris);
+        lv = findViewById(R.id.edittext);
+        arrayList = new ArrayList<String>();
+        adapter = new AdapterP(this, arrayList);
+        fav.setOnClickListener(new View.OnClickListener() {
 
 
-    };
+            @Override
+            public void onClick(View v) {
+                String uniqueId = UUID.randomUUID().toString();
+                Toast.makeText(Description.this, "Clic", Toast.LENGTH_SHORT).show();
+                Log.d(Favoris.TAG, "Click");
+                lv.setAdapter(Favoris.adapter);
+                Favoris.arrayList.add(uniqueId);
+                Favoris.adapter.notifyDataSetChanged();
 
+            }
+
+
+        });
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflaterMenu = getMenuInflater();
         inflaterMenu.inflate(R.menu.monmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item1) {
+        switch (item1.getItemId()) {
+            case R.id.fav:
+                Intent myIntent = new Intent(Description.this, Favoris.class);
+                Description.this.startActivity(myIntent);
+                break;
+
+            case R.id.acc:
+                Intent myacc = new Intent(Description.this, accueil.class);
+                Description.this.startActivity(myacc);
+                break;
+        }
         return true;
     }
 
